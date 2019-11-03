c0522127b8da68745fb66e6c3d7725579263941b
NathanPB <nathan.pbombana@gmail.com>
2019-11-03 18:10:54 -0300

    Fixed ParseError when the request returns empty body (shitty internet connections)

--- plasma5-genial/contents/code/youtube-helper.js
+++ plasma5-genial/contents/code/youtube-helper.js
@@ -2,6 +2,12 @@
 
 const getVideoDataFromUrl = (url) => new Promise((resolve, reject) => {
     APIHelper.httpRequest(`https://www.youtube.com/oembed?format=json&url=${encodeURIComponent(url)}`, 'GET')
-        .then(response => resolve(JSON.parse(response)))
+        .then(response => {
+            try {
+                resolve(JSON.parse(response))
+            } catch(ex) {
+                reject(ex)
+            }
+        })
         .catch(reject)
 })
