9f99e236f47b9fc1198cf2f3b4e757edfe23aae3
NathanPB <nathan.pbombana@gmail.com>
2019-11-09 21:55:32 -0300

    Added Babel and build scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 mkdir -p build
-zip -r build/plasma5-genial.plasmoid plasma5-genial/*
+rm -rf build/temp build/plasma5-genial build/sources
+cp -R plasma5-genial build
+mv build/plasma5-genial build/temp
+
+find build/temp -type f -not -path '*/\.js' -exec sed -i 's/.import /\/\/.import /g' {} +
+babel --presets env ./build/temp/ -x '.js'  --retain-lines -d ./build/sources
+find build/sources -type f -not -path '*/\.js' -exec sed -i 's/\/\/.import /.import /g' {} +
+rsync -a --ignore-existing build/temp/ build/sources
+
+rm -rf plasma5-genial.plasmoid build/temp
+cd build/sources
+zip -r ../plasma5-genial.plasmoid *
